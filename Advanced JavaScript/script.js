c25a3a3c0d5ab3e606ad6a65562b5ca604555734
Ryan Wynn <ryan.james.wynn@gmail.com>
2026-07-29 15:48:57 +0400

    let and const are two ways to declare variables in JavaScript, but they have different behaviors and use cases.

--- Advanced JavaScript/script.js
+++ Advanced JavaScript/script.js
@@ -4,3 +4,22 @@ condition ? expressionIfTrue : expressionIfFalse;
 let age = 18;
 let canVote = age >= 18 ? 'Yes, you can vote.' : 'No, you cannot vote.';
 console.log(canVote); // Output: Yes, you can vote.
+
+// Switch Statement
+function getDayName(dayNumber) {
+  switch (dayNumber) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+  }
+  return 'Invalid day number';
+}
+
+// let + const
+// let allows you to declare variables that are limited in scope to the block, statement, or expression on which it is used.
+// const allows you to declare variables that cannot be reassigned after their initial assignment.
