776008de292530260b26bd094e514734e280e7b5
Ryan Wynn <ryan.james.wynn@gmail.com>
2026-07-29 15:34:01 +0400

    Advanced Javascript

---
+++ Advanced JavaScript/script.js
@@ -0,0 +1,6 @@
+// Ternary Operator
+condition ? expressionIfTrue : expressionIfFalse;
+// Example
+let age = 18;
+let canVote = age >= 18 ? 'Yes, you can vote.' : 'No, you cannot vote.';
+console.log(canVote); // Output: Yes, you can vote.
