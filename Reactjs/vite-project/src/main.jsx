d6dba96a7ff59453caddacefcd3a0569bc84ba39
Ryan Wynn <ryan.james.wynn@gmail.com>
2026-08-06 13:12:09 +0400

    Props are a way of passing data from parent components to child components in React. They allow you to customize the behavior and appearance of a component by providing it with specific values. Props are read-only, meaning that a child component cannot modify the props it receives from its parent.

--- Reactjs/vite-project/src/main.jsx
+++ Reactjs/vite-project/src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import Hello from './Hello.jsx';
 import 'tachyons'; // import tachyons for styling
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Hello />
+    <Hello greeting={['Hello', 'React Ninja']} />
   </StrictMode>
 );
